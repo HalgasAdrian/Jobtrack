c3f72bd5161b112750300b94ddcca49380d3fa7f
ArvindParekh <aruparekh@gmail.com>
2025-11-03 16:23:10 -0500

    feat: add auth controllers for register, login, and user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,16 +1,15 @@
 import express from "express"
+import {login, register, getUser} from "../controllers/authController.js";
 
 const router = express.Router();
 
 // public routes
 router.post("/register", register);
 router.post("/login", login);
-router.post("/logout", logout);
+// router.post("/logout", logout);
 
 // protected route (get user's profile)
-router.get("/me", authMiddleware, async (req, res) => {
-
-});
+router.get("/me", authMiddleware, getUser);
 
 export default router;
 
